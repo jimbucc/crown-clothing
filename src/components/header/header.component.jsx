aa5b3831bdada0ac3b0b8c9a182461a2e6a0b598
jimbucc <jimbucc@sbcglobal.net>
2019-09-09 08:01:55 -0400

    added redux and setup store for current user

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -38,4 +40,8 @@ const Header = ({ currentUser }) => (
     // </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
