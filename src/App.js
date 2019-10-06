81a244d16be4bbc0f6510a07dbb0147c6fc17ee6
jimbucc <jimbucc@sbcglobal.net>
2019-10-06 11:01:10 -0400

    Converted user shop and cart to use redux sagas

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+//import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+//import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+import { checkUserSession } from './redux/user/user.actions';
 
 import './App.css';
 
@@ -21,25 +22,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser /*, collectionsArray */ } = this.props;
+    const { checkUserSession } = this.props;
+    checkUserSession();
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        // Get data related to this user
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          });
-        });
-
-      }
-      setCurrentUser(userAuth);
-    })
   }
 
   componentWillUnmount() {
@@ -72,7 +57,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component'
-import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
+//import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
@@ -18,12 +21,9 @@ class SignIn extends Component {
         event.preventDefault();
 
         const { email, password } = this.state;
+        const { emailSignInStart } = this.props;
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-        }
-        catch(err) { console.log(err); }
+        emailSignInStart(email, password);
     }
 
     handleChange = event => {
@@ -32,20 +32,21 @@ class SignIn extends Component {
     }
 
     render() {
+        const { googleSignInStart } = this.props;
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                        name="email" 
-                        type="email" 
+                    <FormInput
+                        name="email"
+                        type="email"
                         value={this.state.email}
-                        handleChange={this.handleChange} 
+                        handleChange={this.handleChange}
                         label='email'
                         required />
-                    
+
                     <FormInput
                         name="password"
                         type="password"
@@ -54,11 +55,9 @@ class SignIn extends Component {
                         label='password'
                         required />
                     <div className='buttons'>
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                    
-                        {' '}
-                        Sign In with Google
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>
+                            Sign In with Google
                     </CustomButton>
                     </div>
                 </form>
@@ -68,4 +67,10 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => 
+        dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn)
